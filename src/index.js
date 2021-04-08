2a7988df6f0be5b6e25d566d02930118c8e41ef2
Nestor <nestor.venegas95@gmail.com>
2021-04-07 19:43:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//Basic Deliverables
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("oof");
+  //Identify the form that is taking the user's content.
+  let taskForm = document.querySelector("#create-task-form");
+
+  //Identify where we want the submission to go.
+  let taskList = document.querySelector("#tasks")
+
+  //Create an event listener for our form
+  taskForm.addEventListener("submit", function(event){
+
+    //Allow our form to ignore its default refresh setting.
+    event.preventDefault();
+    
+    //Record the value of the user's input. 
+    let newTask = document.querySelector("#new-task-description").value;
+
+    //Adds recorded values to our task list.
+    taskList.innerHTML += `<li>${newTask}</li>`
+
+
+
+
+  })
+  
+
+ 
 });
